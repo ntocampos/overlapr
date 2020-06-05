7a09572b70a850e126d15b62dbba50e2dd1e18b7
Moisés Neto <ntocampos@gmail.com>
2020-06-05 14:59:47 -0300

    Tweaks areRangesOverlapping to be more user friendly

--- src/helpers/areRangesOverlapping.ts
+++ src/helpers/areRangesOverlapping.ts
@@ -1,9 +1,6 @@
-const areRangesOverlapping = (
-  start1: number,
-  end1: number,
-  start2: number,
-  end2: number
-) => {
+type Range = [number, number]
+
+const areRangesOverlapping = ([start1, end1]: Range, [start2, end2]: Range) => {
   if (!(start1 <= end1 && start2 <= end2)) {
     throw new RangeError('Interval ends must be before its start')
   }
