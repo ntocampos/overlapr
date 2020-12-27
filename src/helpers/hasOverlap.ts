f17683fc80fba0da3cfac533239ac7965226eac7
Moisés Neto <ntocampos@gmail.com>
2020-12-27 13:59:49 -0300

    Tweaks hasOverlap helper

--- src/helpers/hasOverlap.ts
+++ src/helpers/hasOverlap.ts
@@ -5,7 +5,9 @@ const hasOverlap = (
   [start2, end2]: SimpleRange
 ) => {
   if (!(start1 <= end1 && start2 <= end2)) {
-    throw new RangeError('Interval ends must be before its start')
+    throw new RangeError(
+      `Interval ends must be before its start: [${start1}, ${end1}], [${start2}, ${end2}]`
+    )
   }
 
   return start1 < end2 && start2 < end1
