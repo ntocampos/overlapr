ccadbb2cdce1e6a83673d77bcd2ba38a6b4d1cb5
Moisés Neto <ntocampos@gmail.com>
2020-12-27 14:00:10 -0300

    Implements assignDepth core function and specs

---
+++ specs/core/assignDepth.spec.ts
@@ -0,0 +1,89 @@
+import prepareInput from '@/core/prepareInput'
+import assignDepth from '@/core/assignDepth'
+
+describe('assignDepth', () => {
+  let items
+
+  /*
+      -----  -----  -----
+     |  a  ||  b  ||  c  |
+      -----  -----  -----
+  */
+  describe('non-overlapping ranges', () => {
+    beforeEach(
+      () =>
+        (items = prepareInput([
+          { id: 'a', start: 0, end: 5 },
+          { id: 'b', start: 5, end: 10 },
+          { id: 'c', start: 10, end: 15 },
+        ]))
+    )
+
+    test('assigns same depth to all ranges', () => {
+      const results = assignDepth(items)
+
+      expect(results['a'].depth).toEqual(0)
+      expect(results['b'].depth).toEqual(0)
+      expect(results['c'].depth).toEqual(0)
+    })
+  })
+
+  /*
+    Scenario 2: two independent events being overlapped by a third
+      -----
+     |  a  |
+      -----
+      -----
+     |  b  |
+      -----
+      -----
+     |  c  |
+      -----
+  */
+  describe('all ranges overlapping', () => {
+    beforeEach(
+      () =>
+        (items = prepareInput([
+          { id: 'a', start: 0, end: 5 },
+          { id: 'b', start: 0, end: 5 },
+          { id: 'c', start: 0, end: 5 },
+        ]))
+    )
+
+    test('assigns different depths to each', () => {
+      const results = assignDepth(items)
+
+      expect(results['a'].depth).toEqual(0)
+      expect(results['b'].depth).toEqual(1)
+      expect(results['c'].depth).toEqual(2)
+    })
+  })
+
+  /*
+    Scenario 3: two independent events being overlapped by a third
+      -----
+     |  a  | -----
+      ----- |  c  |
+      ----- |     |
+     |  b  | -----
+      -----
+  */
+  describe('two independent ranges overlapped by a third', () => {
+    beforeEach(
+      () =>
+        (items = prepareInput([
+          { id: 'a', start: 0, end: 5 },
+          { id: 'b', start: 5, end: 10 },
+          { id: 'c', start: 3, end: 8 },
+        ]))
+    )
+
+    test('assigns same depth to independent ones', () => {
+      const results = assignDepth(items)
+
+      expect(results['a'].depth).toEqual(0)
+      expect(results['b'].depth).toEqual(0)
+      expect(results['c'].depth).toEqual(1)
+    })
+  })
+})
---
+++ src/core/assignDepth.ts
@@ -0,0 +1,34 @@
+import hasOverlap from '@/helpers/hasOverlap'
+import type { OverlapItem, OverlapData } from '@/types'
+
+const assignDepth = (data: OverlapData) => {
+  const items = data._ordered
+  let groups: OverlapItem[][] = []
+
+  items.forEach((item) => {
+    for (let [index, group] of groups.entries()) {
+      const hasConflict = group.some((groupItem) => {
+        return hasOverlap(
+          [item.start, item.end],
+          [groupItem.start, groupItem.end]
+        )
+      })
+
+      if (!hasConflict) {
+        item.depth = index
+        group.push(item)
+      }
+    }
+
+    if (isNil(item.depth)) {
+      groups.push([item])
+      item.depth = groups.length - 1
+    }
+  })
+
+  return data
+}
+
+const isNil = (value: any) => value === undefined || value === null
+
+export default assignDepth
