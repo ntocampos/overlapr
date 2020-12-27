e972c74aed698420e5ec178998b4b993b32ac624
Moisés Neto <ntocampos@gmail.com>
2020-12-27 14:00:29 -0300

    Adds desired API to entry file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 export const Greeter = (name: string) => `Hello ${name}`
+
+/*
+  const ranges = any[]
+  const inputs = prepareInput(ranges)
+  const mediator = pipe(
+    assignDepth,
+    assignOverlaps,
+    assignContextualDepth,
+  )(inputs)
+
+  mediator.getAll()
+  mediator.get(rangeId): Result[]
+  mediator.getDepth(rangeId): Float (percentage)
+  mediator.getWidth(rangeId): Integer (span)
+  mediator.getHeight(rangeId): Integer (span)
+
+  Result = {
+    id: String,
+    original: Object,
+    depth: Float,
+    width: Integer,
+    height: Integer,
+    overlaps: String[],
+    context: Integer
+  }
+
+*/
