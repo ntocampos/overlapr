11ecba92edbe54ae5301109ebc6503015d80a466
Moisés Neto <ntocampos@gmail.com>
2020-12-26 22:17:50 -0300

    Adds prepareInput module

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,30 @@
 export type SimpleRange = [number, number]
 
 export type Config = {
-  getId: (item: object) => string
-  getStart: (item: object) => number
-  getEnd: (item: object) => number
+  getId: (item: any) => string
+  getStart: (item: any) => number
+  getEnd: (item: any) => number
 }
 
 export type Input = {
   id?: string
   start?: number
   end?: number
+}
+
+export type OverlapItem = {
+  _original: Input
+  id: string
+  start: number
+  end: number
+  depth?: number
+  width?: number
+  height?: number
+  overlaps?: string[]
+  context?: number
+}
+
+export type OverlapData = {
+  [key: number]: OverlapItem
+  _ordered?: OverlapItem[]
 }
