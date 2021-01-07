1483685c0b2268841643c85e2b255ec8632b88ba
Moisés Neto <ntocampos@gmail.com>
2021-01-07 16:18:53 -0300

    Implements assignWidth core function

---
+++ src/core/assignWidth.ts
@@ -0,0 +1,22 @@
+import { OverlapData } from 'src/types'
+
+const assignWidth = (data: OverlapData) => {
+  const items = data._ordered
+
+  items.forEach((item) => {
+    const nextConflictingDepths = item.conflicts
+      .filter((id) => data[id].depth > item.depth)
+      .map((id) => data[id].depth)
+
+    const startingDepth = item.depth
+    const endingDepth = nextConflictingDepths.length
+      ? Math.min(...nextConflictingDepths)
+      : item.context
+
+    item.width = (endingDepth - startingDepth) / item.context
+  })
+
+  return data
+}
+
+export default assignWidth
