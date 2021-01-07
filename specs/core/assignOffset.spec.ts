62b8eb319fd46d3e0caeffab4e997dac0a26ab71
Moisés Neto <ntocampos@gmail.com>
2021-01-07 16:18:30 -0300

    Implements assignOffset core function

---
+++ specs/core/assignOffset.spec.ts
@@ -0,0 +1,170 @@
+import prepareInput from '../../src/core/prepareInput'
+import assignDepth from '../../src/core/assignDepth'
+import assignConflicts from '../../src/core/assignConflicts'
+import assignContext from '../../src/core/assignContext'
+import assignOffset from '../../src/core/assignOffset'
+
+describe('assignOffset', () => {
+  let items
+
+  /* Scenario 1: non-overlapping ranges
+      -----
+     |  a  |
+      -----
+      -----
+     |  b  |
+      -----
+      -----
+     |  c  |
+      -----
+  */
+  describe('non-overlapping ranges', () => {
+    beforeEach(() => {
+      items = prepareInput([
+        { id: 'a', start: 0, end: 5 },
+        { id: 'b', start: 5, end: 10 },
+        { id: 'c', start: 10, end: 15 },
+      ])
+
+      assignDepth(items)
+      assignConflicts(items)
+      assignContext(items)
+    })
+
+    test('assigns empty arrays to all ranges', () => {
+      const results = assignOffset(items)
+
+      expect(results['a'].offset).toEqual(0)
+      expect(results['b'].offset).toEqual(0)
+      expect(results['c'].offset).toEqual(0)
+    })
+  })
+
+  /* Scenario 2: all ranges overlapping
+      -----  -----  -----
+     |  a  ||  b  ||  c  |
+      -----  -----  -----
+  */
+  describe('all ranges overlapping', () => {
+    beforeEach(() => {
+      items = prepareInput([
+        { id: 'a', start: 0, end: 5 },
+        { id: 'b', start: 0, end: 5 },
+        { id: 'c', start: 0, end: 5 },
+      ])
+
+      assignDepth(items)
+      assignConflicts(items)
+      assignContext(items)
+    })
+
+    test('assigns context correctly', () => {
+      const results = assignOffset(items)
+
+      expect(results['a'].offset).toEqual(0)
+      expect(results['b'].offset).toEqual(1 / 3)
+      expect(results['c'].offset).toEqual(2 / 3)
+    })
+  })
+
+  /* Scenario 3: two independent ranges overlapped by a third
+      -----
+     |  a  | -----
+      ----- |  c  |
+      ----- |     |
+     |  b  | -----
+      -----
+  */
+  describe('two independent ranges overlapped by a third', () => {
+    beforeEach(() => {
+      items = prepareInput([
+        { id: 'a', start: 0, end: 5 },
+        { id: 'b', start: 5, end: 10 },
+        { id: 'c', start: 3, end: 8 },
+      ])
+
+      assignDepth(items)
+      assignConflicts(items)
+      assignContext(items)
+    })
+
+    test('assigns context correctly', () => {
+      const results = assignOffset(items)
+
+      expect(results['a'].offset).toEqual(0)
+      expect(results['b'].offset).toEqual(0)
+      expect(results['c'].offset).toEqual(1 / 2)
+    })
+  })
+
+  /* Scenario 4: two independent clusters
+      -----
+     |  a  | -----
+      ----- |  c  |
+      ----- |     |
+     |  b  | -----
+      -----
+      -----
+     |  d  |
+      -----
+  */
+  describe('two independent clusters', () => {
+    beforeEach(() => {
+      items = prepareInput([
+        { id: 'a', start: 0, end: 5 },
+        { id: 'b', start: 5, end: 10 },
+        { id: 'c', start: 3, end: 8 },
+        { id: 'd', start: 10, end: 15 },
+      ])
+
+      assignDepth(items)
+      assignConflicts(items)
+      assignContext(items)
+    })
+
+    test('assigns context correctly', () => {
+      const results = assignOffset(items)
+
+      expect(results['a'].offset).toEqual(0)
+      expect(results['b'].offset).toEqual(0)
+      expect(results['c'].offset).toEqual(1 / 2)
+      expect(results['d'].offset).toEqual(0)
+    })
+  })
+
+  /* Scenario 5: convoluted cluster
+      -----
+     |  a  | -----
+      ----- |  c  | -----
+      ----- |     ||  d  |
+     |  b  | -----  -----
+      -----  -----
+            |  e  |
+             -----
+  */
+  describe('one convoluted cluster', () => {
+    beforeEach(() => {
+      items = prepareInput([
+        { id: 'a', start: 0, end: 5 },
+        { id: 'b', start: 5, end: 10 },
+        { id: 'c', start: 3, end: 8 },
+        { id: 'd', start: 5, end: 8 },
+        { id: 'e', start: 9, end: 15 },
+      ])
+
+      assignDepth(items)
+      assignConflicts(items)
+      assignContext(items)
+    })
+
+    test('assigns context correctly', () => {
+      const results = assignOffset(items)
+
+      expect(results['a'].offset).toEqual(0)
+      expect(results['b'].offset).toEqual(0)
+      expect(results['c'].offset).toEqual(1 / 3)
+      expect(results['d'].offset).toEqual(2 / 3)
+      expect(results['e'].offset).toEqual(1 / 3)
+    })
+  })
+})
---
+++ src/core/assignOffset.ts
@@ -0,0 +1,10 @@
+import { OverlapData } from 'src/types'
+
+const assignOffset = (data: OverlapData) => {
+  const items = data._ordered
+  items.forEach((item) => (item.offset = item.depth / item.context))
+
+  return data
+}
+
+export default assignOffset
