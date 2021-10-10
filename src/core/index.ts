69a424080e5355da983023ffed6e4b788556cc22
Moisés Neto <ntocampos@gmail.com>
2021-10-10 17:28:32 -0300

    Implements the main export

---
+++ src/core/index.ts
@@ -0,0 +1,15 @@
+import assignDepth from './assignDepth'
+import assignConflicts from './assignConflicts'
+import assignContext from './assignContext'
+import assignOffset from './assignOffset'
+import assignWidth from './assignWidth'
+import assignHeight from './assignHeight'
+
+export {
+  assignDepth,
+  assignConflicts,
+  assignContext,
+  assignOffset,
+  assignWidth,
+  assignHeight,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export const Greeter = (name: string) => `Hello ${name}`
+import pipe from './helpers/pipe'
+import {
+  assignDepth,
+  assignConflicts,
+  assignContext,
+  assignOffset,
+  assignWidth,
+  assignHeight,
+} from './core'
+import prepareInput from './core/prepareInput'
+
+import { Config } from './types'
+
+export const processData = (userInput: object[], config?: Config | object) => {
+  const data = prepareInput(userInput, config)
+
+  return pipe(
+    assignDepth,
+    assignConflicts,
+    assignContext,
+    assignOffset,
+    assignWidth,
+    assignHeight
+  )(data)
+}
+
+export default { processData }
